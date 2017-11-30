917cdf450e79861dda1660b29a8569e7d6513f4f
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2017-11-29 22:07:04 -0200

    Add the first view

--- components/Decks/index.js
+++ components/Decks/index.js
@@ -1,15 +1,40 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import { container } from './../../utils/styles'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { black, darkGray } from './../../utils/colors'
+
+const data = [
+  { name: 'React', cards: 3 },
+  { name: 'Redux', cards: 5 },
+  { name: 'Javascript', cards: 9 }
+]
 
 const Decks = () => (
-  <View style={styles.container}>
-    <Text>Decks view</Text>
+  <View>
+    {data.map(item => (
+      <TouchableOpacity key={item.name} style={styles.item}>
+        <Text style={styles.itemTitle}>{item.name}</Text>
+        <Text style={styles.itemSubtitle}>{item.cards} cards</Text>
+      </TouchableOpacity>
+    ))}
   </View>
 )
 
 const styles = StyleSheet.create({
-  container
+  item: {
+    height: 100,
+    borderBottomColor: darkGray,
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  itemTitle: {
+    fontSize: 22,
+    color: black
+  },
+  itemSubtitle: {
+    fontSize: 13,
+    color: darkGray
+  }
 })
 
 export default Decks
---
+++ utils/colors.js
@@ -0,0 +1,4 @@
+export const black = '#333'
+export const green = '#c2c57f'
+export const red = '#a13939'
+export const darkGray = '#909090'
